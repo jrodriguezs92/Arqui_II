416afa3024765a0930b25475b997c170281f4532
jrodriguezs92 <jrodriguezs0292@gmail.com>
2019-05-03 16:33:04 -0600

    estandar modificado

--- Proyecto2/include/pipeline.h
+++ Proyecto2/include/pipeline.h
@@ -1,93 +1,110 @@
+/**
+ * Copyright (C) 2019 
+ * Área Académica de Ingeniería en Computadoras, ITCR, Costa Rica
+ *
+ * Definicion de segmentos para el procesador pipeline vectorial
+ *
+ * @Author: Jeremy Rodriguez
+ * @Date  : 03.05.2019
+ */
+
+
+#ifndef PIPELINE_H_INCLUDED
+#define PIPELINE_H_INCLUDED
+
 #include <iostream>
 #include <vector>
 #include <string>
 
-using namespace std;
+namespace arqII 
+{
 
-// Declaracion de componentes estaticos
+    // Declaracion de componentes estaticos
 
-extern vector<string> memInstr;                         // Memoria de instruciones
-extern vector<vector<unsigned short int>> memV;         // Memoria de datos vectoriales
-extern vector<short int> memE;                          // Memoria de datos escalares
-extern vector< vector<unsigned short int>> bancRegsV;   // Banco de registros vectoriales
-extern vector<unsigned short int> bancRegsE;            // Banco de registros escalares
+    extern std::vector<std::string> memInstr;                       // Memoria de instruciones
+    extern std::vector<std::vector<unsigned short int>> memV;       // Memoria de datos vectoriales
+    extern std::vector<short int> memE;                             // Memoria de datos escalares
+    extern std::vector<std::vector<unsigned short int>> bancRegsV;  // Banco de registros vectoriales
+    extern std::vector<unsigned short int> bancRegsE;               // Banco de registros escalares
 
-// Registros pipeline
+    // Registros pipeline
 
-struct IFID {
-    string instruccion;                                 // Instruccion
-};
+    struct IFID {
+        std::string instruccion;      // Instruccion
+    };
 
-extern struct IFID ifid;
-
-
-struct IDEX {
-    vector<vector<unsigned short int>> DOAV;            // Dato A de salida del banco de registro vectorial
-    vector<vector<unsigned short int>> DOBV;            // Dato A de salida del banco de registro vectorial
-    unsigned short int DOAE;                            // Dato A de salida del banco de registro escalar
-    short int dst1;                                     // Datos dst de [11,8], formatos I y R
-    short int dst2;                                     // Datos dst de [11,7], formato LE
-    short int inm1;                                     // Dato Inm de [7,0], formato I
-    short int inm2;                                     // Dato Inm de [6,0], formato LE
-
-    // Senyales de control
-    struct {
-        unsigned short int RegVWrite;                   // Senyal de control: Escritura en banco de registros vectorial
-        unsigned short int RegVRead;                    // Senyal de control: Lectura en banco de registros vectorial
-        unsigned short int RegEWrite;                   // Senyal de control: Escritura en banco de registros escalar
-        unsigned short int RegERead;                    // Senyal de control: Lectura en banco de registros vectorial
-        unsigned short int MemVWrite;                   // Senyal de control: Escritura en memoria vectorial
-        unsigned short int MemVRead;                    // Senyal de control: Lectura en memoria vectorial
-        unsigned short int MemEWrite;                   // Senyal de control: Escritura en memoria escalar
-        unsigned short int MemERead;                    // Senyal de control: Lectura en memoria escalar
-        short int MuxABancRegV;                         // Senyal de control: MUX a la entrada DirA del banco de registros vectoriales
-        short int MuxBALU;                              // Senyal de control: MUX a la entrada B de la ALU
-        short int MuxWB;                                // Senyal de control: MUX en segmento WB
-    }control;
-};
-
-extern struct IDEX idex;
+    extern struct IFID ifid;
 
 
-struct EXMEM {
-    vector<vector<unsigned short int>> ALUOut;          // Dato resultado de unidad Funcional
-    vector<vector<unsigned short int>> DOAV;            // Dato A de salida del banco de registro vectorial
-    unsigned short int DOAE;                            // Dato A de salida del banco de registro escalar
-    short int dst1;                                     // Datos dst de [11,8], formatos I y R
-    short int dst2;                                     // Datos dst de [11,7], formato LE
-    short int inm2;                                     // Dato Inm de [6,0], formato LE
+    struct IDEX {
+        std::vector<std::vector<unsigned short int>> DOAV;  // Dato A de salida del banco de registro vectorial
+        std::vector<std::vector<unsigned short int>> DOBV;  // Dato A de salida del banco de registro vectorial
+        unsigned short int DOAE;                            // Dato A de salida del banco de registro escalar
+        short int dst1;                                     // Datos dst de [11,8], formatos I y R
+        short int dst2;                                     // Datos dst de [11,7], formato LE
+        short int inm1;                                     // Dato Inm de [7,0], formato I
+        short int inm2;                                     // Dato Inm de [6,0], formato LE
 
-    // Senyales de control
-    struct {
-        unsigned short int RegVWrite;                   // Senyal de control: Escritura en banco de registros vectorial
-        unsigned short int RegVRead;                    // Senyal de control: Lectura en banco de registros vectorial
-        unsigned short int RegEWrite;                   // Senyal de control: Escritura en banco de registros escalar
-        unsigned short int RegERead;                    // Senyal de control: Lectura en banco de registros vectorial
-        unsigned short int MemVWrite;                   // Senyal de control: Escritura en memoria vectorial
-        unsigned short int MemVRead;                    // Senyal de control: Lectura en memoria vectorial
-        unsigned short int MemEWrite;                   // Senyal de control: Escritura en memoria escalar
-        unsigned short int MemERead;                    // Senyal de control: Lectura en memoria escalar
-    }control;
+        // Senyales de control
+        struct {
+            unsigned short int RegVWrite;                   // Senyal de control: Escritura en banco de registros vectorial
+            unsigned short int RegVRead;                    // Senyal de control: Lectura en banco de registros vectorial
+            unsigned short int RegEWrite;                   // Senyal de control: Escritura en banco de registros escalar
+            unsigned short int RegERead;                    // Senyal de control: Lectura en banco de registros vectorial
+            unsigned short int MemVWrite;                   // Senyal de control: Escritura en memoria vectorial
+            unsigned short int MemVRead;                    // Senyal de control: Lectura en memoria vectorial
+            unsigned short int MemEWrite;                   // Senyal de control: Escritura en memoria escalar
+            unsigned short int MemERead;                    // Senyal de control: Lectura en memoria escalar
+            short int MuxABancRegV;                         // Senyal de control: MUX a la entrada DirA del banco de registros vectoriales
+            short int MuxBALU;                              // Senyal de control: MUX a la entrada B de la ALU
+            short int MuxWB;                                // Senyal de control: MUX en segmento WB
+        }control;
+    };
 
-};
-
-extern struct EXMEM exmem;
+    extern struct IDEX idex;
 
 
-struct MEMWB {
-    vector<vector<unsigned short int>> ALUOut;          // Dato resultado de unidad Funcional
-    vector<vector<unsigned short int>> DOMemV;          // Dato de salida la memoria vectorial
-    unsigned short int DOMemE;                          // Dato de salida la memoria escalar
-    short int dst1;                                     // Datos dst de [11,8], formatos I y R
-    short int dst2;                                     // Datos dst de [11,7], formato LE
+    struct EXMEM {
+        std::vector<std::vector<unsigned short int>> ALUOut;    // Dato resultado de unidad Funcional
+        std::vector<std::vector<unsigned short int>> DOAV;      // Dato A de salida del banco de registro vectorial
+        unsigned short int DOAE;                                // Dato A de salida del banco de registro escalar
+        short int dst1;                                         // Datos dst de [11,8], formatos I y R
+        short int dst2;                                         // Datos dst de [11,7], formato LE
+        short int inm2;                                         // Dato Inm de [6,0], formato LE
 
-    // Senyales de control
-    struct {
-        unsigned short int RegVWrite;                   // Senyal de control: Escritura en banco de registros vectorial
-        unsigned short int RegVRead;                    // Senyal de control: Lectura en banco de registros vectorial
-        unsigned short int RegEWrite;                   // Senyal de control: Escritura en banco de registros escalar
-        unsigned short int RegERead;                    // Senyal de control: Lectura en banco de registros vectorial
-    }control;
-};
+        // Senyales de control
+        struct {
+            unsigned short int RegVWrite;                   // Senyal de control: Escritura en banco de registros vectorial
+            unsigned short int RegVRead;                    // Senyal de control: Lectura en banco de registros vectorial
+            unsigned short int RegEWrite;                   // Senyal de control: Escritura en banco de registros escalar
+            unsigned short int RegERead;                    // Senyal de control: Lectura en banco de registros vectorial
+            unsigned short int MemVWrite;                   // Senyal de control: Escritura en memoria vectorial
+            unsigned short int MemVRead;                    // Senyal de control: Lectura en memoria vectorial
+            unsigned short int MemEWrite;                   // Senyal de control: Escritura en memoria escalar
+            unsigned short int MemERead;                    // Senyal de control: Lectura en memoria escalar
+        }control;
+    };
 
-extern struct MEMWB memwb;
+    extern struct EXMEM exmem;
+
+
+    struct MEMWB {
+        std::vector<std::vector<unsigned short int>> ALUOut;    // Dato resultado de unidad Funcional
+        std::vector<std::vector<unsigned short int>> DOMemV;    // Dato de salida la memoria vectorial
+        unsigned short int DOMemE;                              // Dato de salida la memoria escalar
+        short int dst1;                                         // Datos dst de [11,8], formatos I y R
+        short int dst2;                                         // Datos dst de [11,7], formato LE
+
+        // Senyales de control
+        struct {
+            unsigned short int RegVWrite;                       // Senyal de control: Escritura en banco de registros vectorial
+            unsigned short int RegVRead;                        // Senyal de control: Lectura en banco de registros vectorial
+            unsigned short int RegEWrite;                       // Senyal de control: Escritura en banco de registros escalar
+            unsigned short int RegERead;                        // Senyal de control: Lectura en banco de registros vectorial
+        }control;
+    };
+
+    extern struct MEMWB memwb;
+
+} //namespaci arquiII
+#endif
